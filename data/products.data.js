57528deef3500fa0ad893f423aa8585edf6cae85
abood <abdulrahman.mater.08@gmail.com>
2026-09-03 15:01:33 +0300

    complete shop page

---
+++ data/products.data.js
@@ -0,0 +1,111 @@
+// products.data.js
+
+export const products = [
+    {
+        id: 1,
+        title: "House Blend",
+        category: "coffee",
+        src: "images/Amaya logo-C4B59E.png",
+        price: {
+            min: 19,
+            max: 27,
+        },
+        rating: 4.5,
+    },
+
+    {
+        id: 2,
+        title: "Espresso",
+        category: "coffee",
+        src: "images/logo.png",
+        price: {
+            min: 15,
+            max: 20,
+        },
+        rating: 4.8,
+    },
+
+    {
+        id: 3,
+        title: "Cold Brew",
+        category: "cold-brew",
+        src: "images/logo.png",
+        price: {
+            min: 18,
+            max: 25,
+        },
+        rating: 4.2,
+    },
+
+    {
+        id: 4,
+        title: "Cold Brew",
+        category: "cold-brew",
+        src: "images/logo.png",
+        price: {
+            min: 18,
+            max: 25,
+        },
+        rating: 4.6,
+    },
+
+    {
+        id: 5,
+        title: "Cold Brew",
+        category: "cold-brew",
+        src: "images/logo.png",
+        price: {
+            min: 18,
+            max: 25,
+        },
+        rating: 4.4,
+    },
+
+    {
+        id: 6,
+        title: "Decaf Coffee",
+        category: "decaf",
+        src: "images/logo.png",
+        price: {
+            min: 17,
+            max: 23,
+        },
+        rating: 4.1,
+    },
+
+    {
+        id: 7,
+        title: "Coffee Mug",
+        category: "merchandise",
+        src: "images/logo.png",
+        price: {
+            min: 10,
+            max: 15,
+        },
+        rating: 4.7,
+    },
+
+    {
+        id: 8,
+        title: "Coffee Mug",
+        category: "merchandise",
+        src: "images/logo.png",
+        price: {
+            min: 10,
+            max: 15,
+        },
+        rating: 4.3,
+    },
+
+    {
+        id: 9,
+        title: "Coffee Mug",
+        category: "merchandise",
+        src: "images/logo.png",
+        price: {
+            min: 10,
+            max: 15,
+        },
+        rating: 4.9,
+    },
+];

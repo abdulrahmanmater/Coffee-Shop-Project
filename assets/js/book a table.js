2928a0df862caaa1a7383d9bef0edc65292852f3
ziadalaa87654-dotcom <ziad.alaa87654@gmail.com>
2026-09-09 00:31:17 +0300

    final

---
+++ assets/js/book a table.js
@@ -0,0 +1,4 @@
+document.getElementById("bookingForm").addEventListener("submit", function () {
+  alert("We will confirm your reservation within 24 hours via Email.");
+  this.reset();
+});
---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const train = document.getElementById("productTrain");
+  const wrapper = document.getElementById("trainWrapper");
+
+  let isPermanentlyStopped = false;
+  let isHovered = false;
+  let scrollSpeed = 0.5;
+
+  train.innerHTML += train.innerHTML + train.innerHTML;
+
+  function startTrain() {
+    if (!isPermanentlyStopped && !isHovered) {
+      wrapper.scrollLeft += scrollSpeed;
+    }
+
+    animationId = requestAnimationFrame(startTrain);
+  }
+
+  startTrain();
+
+  const cards = train.querySelectorAll(".product-card-item");
+  cards.forEach((card) => {
+    card.addEventListener("mouseenter", function () {
+      isHovered = true;
+    });
+    card.addEventListener("mouseleave", function () {
+      if (!isPermanentlyStopped) {
+        isHovered = false;
+      }
+    });
+  });
+});
